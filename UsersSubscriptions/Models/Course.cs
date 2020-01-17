566d45e2b079a05dbc1990c6b6a33b9fa44dcb3a
KataytsevYuriy <yuriy.kataytsev@gmail.com>
2020-01-17 10:18:31 +0200

    first commit

---
+++ UsersSubscriptions/Models/Course.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UsersSubscriptions.Models
+{
+    public class Course
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public bool IsActive { get; set; }
+        public IEnumerable<string> TeachersId { get; set; }
+    }
+}
---
+++ UsersSubscriptions/Models/Subscription.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UsersSubscriptions.Models
+{
+    public class Subscription
+    {
+        public string Id { get; set; }
+        public DateTime DayStart { get; set; }
+        public DateTime DayFinish { get; set; }
+        public string CourseId { get; set; }
+        public string UserId { get; set; }
+        public bool WasPayed { get; set; }
+        public string CreatedByTeacherId { get; set; }
+        public DateTime CreatedDateTime { get; set; }
+        public string PayedToTeacherId { get; set; }
+        public DateTime PayedDateTime { get; set; }
+    }
+}
